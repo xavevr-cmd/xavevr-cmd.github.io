cc470209351bc19306b41d7f8e4c62d1e0b1020b
Xavevr <xavevr0@gmail.com>
2026-09-04 13:21:20 -0230

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-(() => {
-const title=document.querySelector('.hero-title'), logo=document.querySelector('.logo');
-setInterval(()=>{if(title&&Math.random()<.18){title.style.transform=`translate(${Math.random()*3-1.5}px,${Math.random()*2-1}px)`;setTimeout(()=>title.style.transform='',80)}},1200);
-setInterval(()=>{if(logo&&Math.random()<.12){logo.style.textShadow=`${Math.random()*5-2}px 0 #fff`;setTimeout(()=>logo.style.textShadow='',90)}},1800);
-})();
